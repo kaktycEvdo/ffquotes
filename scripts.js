f048641c5c71bd0243651b8e2ee342b3dcf7dc34
kaktycEvdo <ilya.tretyakov.2005@bk.ru>
2023-09-24 19:01:38 +0700

    fixes and pixes

--- scripts.js
+++ scripts.js
@@ -122,6 +122,64 @@ document.querySelector("button.about_button").addEventListener("click", () => {
 });
 
 
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+    return evt.touches ||      // browser API
+    evt.originalEvent.touches; // jQuery
+}
+                                                                         
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+                                                                         
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+                                                                         
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
+        if ( xDiff > 0 ) {
+            let left = 0;
+            about_button.innerHTML = ruen ? le_text.buttonEN2 : le_text.buttonRU2
+            setActiveLink(true)
+            k = false;
+            about_block.style = "left: "+left+"vw";
+        } else {
+            let left = 100;
+            about_button.innerHTML = ruen ? le_text.buttonEN1 : le_text.buttonRU1
+            setActiveLink()
+            k = true;
+            about_block.style = "left: "+left+"vw";
+            
+        }                       
+    } else {
+        if ( yDiff > 0 && offset < 5 && k) {
+            offset++;
+            setActiveLink();
+        } else if (yDiff < 0 && offset > 0 && k) { 
+            offset--;
+            setActiveLink();
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
+
+
 window.onwheel = function(event) {
     if (!fired && k){
         if (event.deltaY > 0 && offset < 5){
